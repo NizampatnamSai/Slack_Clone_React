f74f30e7799e0e01dd5b0fdacf018da56c321f92
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2022-05-13 13:00:36 +0530

    sidebar & sidebaroptions

--- src/app/Firebase.js
+++ src/app/Firebase.js
@@ -18,6 +18,7 @@ const firebaseConfig = {
   const firebaseApp=firebase.initializeApp(firebaseConfig)
   const db=firebaseApp.firestore()
   const auth=firebase.auth()
+  const provider= new firebase.auth.GoogleAuthProvider();
 
   export  {db,auth};
   
